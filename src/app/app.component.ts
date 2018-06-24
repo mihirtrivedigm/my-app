eebf5a1e5eda3d1daf6d1c028767e57c9f29551a
mihir1611 <mihirtrivedigm@yahoo.in>
2018-06-25 01:10:11 +0530

    First commit
    Welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'મિહિર ત્રિવેદી ના  સામ્રાજ્ય માં આપનું સ્વાગત છે​';
 }
